82fc6bb6dc628ba55d6195c7a77181eda86eae72
joshjnr123 <127718862+joshjnr123@users.noreply.github.com>
2025-09-10 14:06:56 +0800

    ...

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to my React + Vite app 🚀</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>This app is built with React + Vite 💻</p>
+        <p>This is the project website for EE09.</p>
+      </div>
+    );
+  }
+  
+  export default About;
